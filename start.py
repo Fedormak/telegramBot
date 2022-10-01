17ee54ec6a056870fadee5556ba9667f04e4f7a0
Felolru23-laptop <fedmakru@gmail.com>
2022-10-01 17:47:22 +0300

    7.day

--- start.py
+++ start.py
@@ -6,11 +6,18 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def hello(message):
-    bot.send_message(message.chat.id, "Привет! Во мне сидит колькулятор! Если хочешь можешь им воспользоваться)) Командой- /numb")
+    bot.send_message(message.chat.id, "Привет! Во мне сидит калькулятор! Если хочешь можешь им воспользоваться)) Командой- /culc")
 
-@bot.message_handler(commands=['numb'])
-def first_numb(message):
-    bot.send_message(message.chat.id, "Ну как ввиди первое чесло")
+@bot.message_handler(commands=['culc'])
+def helloCulc(message):
+    bot.send_message(message.chat.id,"ввидите первое чесло:")
+    if (message.text == ):
+
+def first_n(message):
+    bot.send_message(message.chat.id,"какое будет действие?(+;-)")
+    a = first_n.message
+    bot.send_message(a)
+
 
 # @bot.message_handler(content_types=['text'])
 #def repit(message):
